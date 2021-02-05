6840f0d69e972c79bc68a25416533cfdf0d4a161
Oskar Kogut <kogutoskar123@gmail.com>
2021-02-05 03:09:50 +0100

    Add withX/Y Vector2 extensions

---
+++ LemFramework/Assets/LemFramework/Code/ExtensionMethods/Vector2Extensions.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace LemFramework.ExtensionMethods
+{
+    public static class Vector2Extensions
+    {
+        public static Vector2 WithX(this Vector2 v, float x)
+        {
+            return new Vector2(x, v.y);
+        }
+
+        public static Vector2 WithY(this Vector2 v, float y)
+        {
+            return new Vector2(v.x, y);
+        }
+    }
+}
