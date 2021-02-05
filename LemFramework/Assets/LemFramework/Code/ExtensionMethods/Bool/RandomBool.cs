7997a1c687e7533a16b1ab43f20e080a072b9616
Oskar Kogut <kogutoskar123@gmail.com>
2021-02-05 02:43:20 +0100

    Add random bool extension method

---
+++ LemFramework/Assets/LemFramework/Code/ExtensionMethods/Bool/RandomBool.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LemFramework.ExtensionMethods.Bool
+{
+    public static class RandomBool
+    {
+        public static bool Random(this bool tbool)
+        {
+            var rand = new Random();
+            return rand.NextDouble() > 0.5;
+        }
+    }
+}
