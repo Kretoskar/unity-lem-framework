f20d3b29f47a1b122ebef8389b43e0aa71e504f0
Oskar Kogut <kogutoskar123@gmail.com>
2021-02-05 02:52:56 +0100

    Add IsBetweenInclusive and IsBetweenExclusive bool extensions

--- LemFramework/Assets/LemFramework/Code/ExtensionMethods/Bool/BoolExtensions.cs
+++ LemFramework/Assets/LemFramework/Code/ExtensionMethods/Bool/BoolExtensions.cs
@@ -9,5 +9,15 @@ namespace LemFramework.ExtensionMethods
             var rand = new Random();
             return rand.NextDouble() > 0.5;
         }
+        
+        public static bool IsBetweenInclusive<T>(this T actual, T lower, T upper) where T : IComparable<T>
+        {
+            return actual.CompareTo(lower) >= 0 && actual.CompareTo(upper) <= 0;
+        }
+        
+        public static bool IsBetweenExclusive<T>(this T actual, T lower, T upper) where T : IComparable<T>
+        {
+            return actual.CompareTo(lower) > 0 && actual.CompareTo(upper) < 0;
+        }
     }
 }
