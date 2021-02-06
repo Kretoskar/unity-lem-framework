e6199d64e852b08392577716b39eca466c32daf2
Oskar <kogutoskar123@gmail.com>
2021-02-06 21:17:52 +0100

    Add State Pattern

---
+++ LemFramework/Assets/LemFramework/Code/Patterns/IState.cs
@@ -0,0 +1,11 @@
+﻿namespace LemFramework.Patterns
+{
+    public interface IState
+    {
+        void Enter(StateMachine sm);
+        void Execute();
+        void Exit();
+        bool Finished { get; }
+        int Priority { get; set; }
+    }
+}
