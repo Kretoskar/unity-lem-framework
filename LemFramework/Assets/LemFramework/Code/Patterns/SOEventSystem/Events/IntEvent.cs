7852aba18e93e9801f47bf91e9434d6a595b475b
Oskar Kogut <kogutoskar123@gmail.com>
2021-02-12 03:38:46 +0100

    Add int event listener

---
+++ LemFramework/Assets/LemFramework/Code/Patterns/SOEventSystem/Events/IntEvent.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LemFramework.Patterns.SOEventSystem
+{
+    [CreateAssetMenu(fileName = "New Int Event", menuName = "Game Events/Int Event")]
+    public class IntEvent : BaseGameEvent<int> { }
+}
---
+++ LemFramework/Assets/LemFramework/Code/Patterns/SOEventSystem/Listeners/IntEventListener.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using LemFramework.Patterns.SOEventSystem;
+using UnityEngine;
+
+public class IntEventListener : BaseGameEventListener<int, IntEvent, UnityIntEvent>
+{
+    
+}
